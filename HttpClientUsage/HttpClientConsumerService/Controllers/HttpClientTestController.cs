21044be2df96ead90e880370507466336e84aa5c
Prasadnair <prasadnair44@gmail.com>
2023-01-07 13:14:47 -0500

    Rightway to use HttpClient

---
+++ HttpClientUsage/HttpClientConsumerService/Controllers/HttpClientTestController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HttpClientConsumerService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HttpClientTestController : ControllerBase
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        
+        public HttpClientTestController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+         }
+
+        [HttpGet]
+        public async Task<string> TestHttpClient()
+        {
+            var url = "http://localhost:5014/WeatherForecast";
+            var httpClient = _httpClientFactory.CreateClient();
+            var response = await httpClient.GetAsync(url);
+            return await response.Content.ReadAsStringAsync();
+        } 
+    }
+}
